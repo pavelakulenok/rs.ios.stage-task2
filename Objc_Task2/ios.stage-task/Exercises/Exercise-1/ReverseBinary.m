3eb2ebc00184f72192ae677261c8fcc16e8c7555
pavelakulenok <67806177+pavelakulenok@users.noreply.github.com>
2021-05-18 01:12:10 +0300

    feat: add solutions

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,13 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    UInt8 result = 0;
+    for (int i = 0; i < 8; i++) {
+        result <<= 1;
+        if (n & 1) { //n % 2 == 1
+            result++;
+        }
+        n >>= 1;
+    }
+    return result;
 }
--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,36 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+
+    NSMutableArray *resultArray = [[NSMutableArray alloc] init];
+    NSMutableArray *numberArray = [[NSMutableArray alloc] init];
+    NSMutableArray *stringArray = [[NSMutableArray alloc] init];
+    
+    for (NSArray *item in array) {
+        if ([item isKindOfClass: [NSArray class]]) {
+            for (int i = 0; i < item.count; i++) {
+                if ([item[i] isKindOfClass: [NSNumber class]]) {
+                    [numberArray addObject: item[i]];
+                } else if ([item[i] isKindOfClass: [NSString class]]) {
+                    [stringArray addObject: item[i]];
+                }
+            }
+        } else {
+            return @[];
+        }
+    }
+
+    [numberArray sortUsingSelector:@selector(compare:)];
+    [stringArray sortUsingSelector:@selector(compare:)];
+
+    if ((numberArray.count != 0) && (stringArray.count != 0)) {
+        [resultArray addObjectsFromArray: @[numberArray]];
+    } else {
+        [resultArray addObjectsFromArray: numberArray];
+        [resultArray addObjectsFromArray: stringArray];
+    }
+
+    return resultArray;
 }
 
 @end
